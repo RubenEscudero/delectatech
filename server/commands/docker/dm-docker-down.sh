50331285938e1da333ddde8a56186572f6395e7b
ruben <rubenescudero00@gmail.com>
2023-03-29 14:09:05 +0200

    dockerize enviroment

--- server/commands/docker/dm-docker-down.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -eq 1 ]; then
-    docker stop $1
-    docker rm $1
-else
-    docker-compose -f /root/delectame/server/containers/docker-compose.yml down
-fi
--- server/commands/docker/dm-docker-ps.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f /root/delectame/server/containers/docker-compose.yml ps
